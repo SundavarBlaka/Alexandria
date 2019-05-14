f323751390d1e2f7ec613f9504646d32c10185b0
ath07 <48419748+ath07@users.noreply.github.com>
2019-05-14 16:24:44 +0200

    Aggiunte le interfaccie

---
+++ Interfaccie/js/profile.js
@@ -0,0 +1,19 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+1.Modify
+*****************************/
+	
+function showModify() {
+		var x = document.getElementById("modify");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		}
+		else {
+			x.style.display = "none";
+		}
+	}
+
+
