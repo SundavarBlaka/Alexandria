b048869e7cd53df282c056d3dda944fb7bc5dd79
ath07 <48419748+ath07@users.noreply.github.com>
2019-05-24 11:01:37 +0200

    Aggiornate le Interfacce

---
+++ Interfacce/js/login.js
@@ -0,0 +1,7 @@
+function showInterests() {
+    var x = document.getElementById("items");
+    if(x.style.display === "none")
+        x.style.display = "block";
+    else
+        x.style.display = "none";   
+}
